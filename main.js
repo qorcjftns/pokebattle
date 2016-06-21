a5d2f18f9d21a31e6ddbfb294b2ac281c7abef6f
Cheulsoon Baek <qorcjftns2@gmail.com>
2016-06-20 21:12:26 -0400

    test for bulbsaur and charmander

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+
+var pokemon = require('./pokemon.js');
+
+var poke = new pokemon.Pokemon(1);
+poke.setIV(31,31,31,31,31,31);
+poke.setEV(252,252,4,0,0,0);
+poke.setLevel(50);
+poke.calculateStats();
+
+var poke2 = new pokemon.Pokemon(4);
+poke2.setIV(31,31,31,31,31,31);
+poke2.setEV(252,252,4,0,0,0);
+poke2.setLevel(50);
+poke2.calculateStats();
